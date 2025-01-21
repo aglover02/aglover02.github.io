db549b37c7b7a292c4ade50bc3939fb348ecd212
aglover02 <ga63717@mail.harpercollege.edu>
2025-01-20 21:37:14 -0600

    edits

--- web200/assignment1/script.js
+++ web200/assignment1/script.js
@@ -1,1 +1,16 @@
+function document() {
+    document.write("Hello Autumn!");
+}
+
+function alert() {
+    window.alert("🎉 Surprise! 🎉")
+}
+
+function change() {
+    document.getElementById('paragraph').innerHTML = "This is being displayed using Javascript.";
+}
+
+function console() {
+    console.log("I hope you're having a great day.");
+}
 
