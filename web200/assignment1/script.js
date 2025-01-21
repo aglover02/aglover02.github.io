c496db75c139e998a572976a0a8a562897cba177
aglover02 <ga63717@mail.harpercollege.edu>
2025-01-20 22:45:05 -0600

    details

--- web200/assignment1/script.js
+++ web200/assignment1/script.js
@@ -1,19 +1,19 @@
-function showDocumentMessage() {
-    document.write("Hello Autumn!");
+function showDocumentMessage() { //displays text
+    document.write("My name is Autumn and I'm learning how to code.");
 }
 
-function showAlertMessage() {
+function showAlertMessage() { //displays an alert
     window.alert("🎉 Surprise! 🎉");
 }
 
-function changeParagraph() {
-    document.getElementById('paragraph').innerHTML = "This is being displayed using Javascript.";
+function changeParagraph() { //defines specified html content
+    document.getElementById('paragraph').innerHTML = "This is being displayed using external Javascript.";
 }
 
-function logConsoleMessage() {
+function logConsoleMessage() { //displays message in console
     console.log("I hope you're having a great day.");
 }
-
+//calls functions
 showDocumentMessage();
 showAlertMessage();
 changeParagraph();
