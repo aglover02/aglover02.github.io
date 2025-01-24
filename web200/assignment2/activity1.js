01ca0b5028ea75e736e96e3e2773e763f446b76f
aglover02 <ga63717@mail.harpercollege.edu>
2025-01-23 21:10:11 -0600

    formatting?

--- web200/assignment2/activity1.js
+++ web200/assignment2/activity1.js
@@ -6,5 +6,7 @@ let monthlyPay = (weeklyPay * 52) / 12;
 let annualPay = weeklyPay * 52;        
 
 document.write("Weekly Gross Pay: $" + weeklyPay.toFixed(2));
-document.write("Monthly Gross Pay: $" + monthlyPay.toFixed(2));
-document.write("Annual Gross Pay: $" + annualPay.toFixed(2));
+
+document.write("    Monthly Gross Pay: $" + monthlyPay.toFixed(2));
+
+document.write("    Annual Gross Pay: $" + annualPay.toFixed(2));
