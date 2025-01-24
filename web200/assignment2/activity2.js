05cbc286947d0247b3abba91f12f76d6e1061eec
aglover02 <ga63717@mail.harpercollege.edu>
2025-01-24 15:35:38 -0600

    small fix 2

--- web200/assignment2/activity2.js
+++ web200/assignment2/activity2.js
@@ -7,8 +7,8 @@ let ageInHours = ageInDays * 24;
 let ageInSeconds = ageInHours * 3600; 
 
 
-document.write(`<p>Age in Years: ${ageInYears} year(s)</p>`);
-document.write(`<p>Age in Months: ${ageInMonths} months</p>`);
-document.write(`<p>Age in Days: ${ageInDays} days</p>`);
-document.write(`<p>Age in Hours: ${ageInHours} hours</p>`);
-document.write(`<p>Age in Seconds: ${ageInSeconds} seconds</p>`);
+document.write("<p>Age in Years: " + ageInYears + " year(s)</p>");
+document.write("<p>Age in Months: " + ageInMonths + " months</p>");
+document.write("<p>Age in Days: " + ageInDays + " days</p>");
+document.write("<p>Age in Hours: " + ageInHours + " hours</p>");
+document.write("<p>Age in Seconds: " + ageInSeconds + " seconds</p>");
