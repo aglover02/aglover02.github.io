cd9c65f8a1516a348ab7bd4fa75d1cac24c33453
aglover02 <ga63717@mail.harpercollege.edu>
2025-01-23 21:37:32 -0600

    activity 2

---
+++ web200/assignment2/activity2.js
@@ -0,0 +1,14 @@
+let ageInYears = 22; 
+
+
+let ageInMonths = ageInYears * 12; 
+let ageInDays = ageInYears * 365; 
+let ageInHours = ageInDays * 24; 
+let ageInSeconds = ageInHours * 3600; 
+
+
+document.write("<p>Age in Years: ${ageInYears} year(s)</p>");
+document.write("<p>Age in Months: ${ageInMonths} months</p>");
+document.write("<p>Age in Days: ${ageInDays} days</p>");
+document.write("<p>Age in Hours: ${ageInHours} hours</p>");
+document.write("<p>Age in Seconds: ${ageInSeconds} seconds</p>");
