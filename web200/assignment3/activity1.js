d64ae92fb9b2449280d315583b7c1510d5a3c828
aglover02 <ga63717@mail.harpercollege.edu>
2025-01-31 16:41:39 -0600

    layout

--- web200/assignment3/activity1.js
+++ web200/assignment3/activity1.js
@@ -3,18 +3,18 @@ function mainShapeAreas() {
     let rectLength = prompt("Enter the length of the rectangle (in feet):");
     let rectWidth = prompt("Enter the width of the rectangle (in feet):");
     let rectangleArea = rectLength * rectWidth;
-    document.write("The area of the rectangle is " + rectangleArea + " square feet.");
+    document.write("<p>The area of the rectangle is " + rectangleArea + " square feet.</p>");
 
     //triangle area
     let triBase = prompt("Enter the base of the triangle (in feet):");
     let triHeight = prompt("Enter the height of the triangle (in feet):");
     let triangleArea = 0.5 * triBase * triHeight;
-    document.write("The area of the triangle is " + triangleArea + " square feet.");
+    document.write("<p>The area of the triangle is " + triangleArea + " square feet.</p>");
 
     //circle area
     let circleRadius = prompt("Enter the radius of the circle (in feet):");
     let circleArea = 3.14 * circleRadius * circleRadius;
-    document.write("The area of the circle is " + circleArea + " square feet.");
+    document.write("<p>The area of the circle is " + circleArea + " square feet.</p>");
 }
 
 mainShapeAreas();
