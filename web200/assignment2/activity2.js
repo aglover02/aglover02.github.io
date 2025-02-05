17b2825bce0e84f0b54608ef76844337c2f18c0d
aglover02 <ga63717@mail.harpercollege.edu>
2025-02-04 20:22:18 -0600

    input fix2

--- web200/assignment2/activity2.js
+++ web200/assignment2/activity2.js
@@ -1,14 +1,16 @@
-let ageInYears = 22; 
+let ageInYears = parseFloat(prompt("Enter your age in years: "));
 
+if (!isNaN(ageInYears) && ageInYears > 0) {
+    let ageInMonths = ageInYears * 12;
+    let ageInDays = ageInYears * 365;
+    let ageInHours = ageInDays * 24;
+    let ageInSeconds = ageInHours * 3600;
 
-let ageInMonths = ageInYears * 12; 
-let ageInDays = ageInYears * 365; 
-let ageInHours = ageInDays * 24; 
-let ageInSeconds = ageInHours * 3600; 
-
-
-document.write("<p>Age in Years: " + ageInYears + " year(s)</p>");
-document.write("<p>Age in Months: " + ageInMonths + " months</p>");
-document.write("<p>Age in Days: " + ageInDays + " days</p>");
-document.write("<p>Age in Hours: " + ageInHours + " hours</p>");
-document.write("<p>Age in Seconds: " + ageInSeconds + " seconds</p>");
+    document.write("<p>Age in Years: " + ageInYears + " year(s)</p>");
+    document.write("<p>Age in Months: " + ageInMonths.toFixed(0) + " months</p>");
+    document.write("<p>Age in Days: " + ageInDays.toFixed(0) + " days</p>");
+    document.write("<p>Age in Hours: " + ageInHours.toFixed(0) + " hours</p>");
+    document.write("<p>Age in Seconds: " + ageInSeconds.toFixed(0) + " seconds</p>");
+} else {
+    document.write("<p>Please enter a valid positive number for your age.</p>");
+}
