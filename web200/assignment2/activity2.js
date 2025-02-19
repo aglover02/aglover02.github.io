61cbad761a636924df649034ee115434c2730492
aglover02 <ga63717@mail.harpercollege.edu>
2025-02-19 15:04:52 -0600

    srgdvsez

--- web200/assignment2/activity2.js
+++ web200/assignment2/activity2.js
@@ -4,7 +4,7 @@ if (!isNaN(ageInYears) && ageInYears > 0) {
     let ageInMonths = ageInYears * 12;
     let ageInDays = ageInYears * 365;
     let ageInHours = ageInDays * 24;
-    let ageInSeconds = ageInHours * 3600;
+    let ageInSeconds = ageInHours * 3600;  
 
     document.write("<p>Age in Years: " + ageInYears + " year(s)</p>");
     document.write("<p>Age in Months: " + ageInMonths.toFixed(0) + " months</p>");
@@ -12,5 +12,5 @@ if (!isNaN(ageInYears) && ageInYears > 0) {
     document.write("<p>Age in Hours: " + ageInHours.toFixed(0) + " hours</p>");
     document.write("<p>Age in Seconds: " + ageInSeconds.toFixed(0) + " seconds</p>");
 } else {
-    document.write("<p>Please enter a valid positive number for your age.</p>");
+    document.write("<p>Please enter a valid positive number for your age.</p>"); 
 }
